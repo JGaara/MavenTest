01d532d58f756e67cbd71b7d0f2dc9f985804c71
jiadawei <1225227815@qq.com>
2016-05-08 23:35:16 +0800

    stash解决冲突之后提交

--- src/main/java/com/jdw/bean/Car.java
+++ src/main/java/com/jdw/bean/Car.java
@@ -1,8 +1,26 @@
 package com.jdw.bean;
 
 public class Car {
-
 	private String brand1;
+	
+	/**
+	 * 刚刚添加了price属性，还未添加get set，不能使用，
+	 * 即不能commit，get set方法预计需要一天才能写完（假设）；
+	 * 这时候有个着急的bug，说brand属性名字要改成brand1，怎么办？
+	 */
+	private double price;
+
+	/**
+	 * 解决冲突，并添加了price的get set方法，注意是先add to index，再commit
+	 * @return
+	 */
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
 
 	public String getBrand1() {
 		return brand1;
