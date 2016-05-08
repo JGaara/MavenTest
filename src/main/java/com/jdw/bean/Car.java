7e0c4d85a9132ded7cd64b08c4aba20f228fedbb
jiadawei <1225227815@qq.com>
2016-05-08 23:06:40 +0800

    为了测试方便，这里添加brand1,与jdw分支的brand进行区别

--- src/main/java/com/jdw/bean/Car.java
+++ src/main/java/com/jdw/bean/Car.java
@@ -1,5 +1,17 @@
 package com.jdw.bean;
 
 public class Car {
+	/**
+	 * 为了测试方便，这里添加brand1,与jdw分支的brand进行区别
+	 */
+	private String brand1;
+
+	public String getBrand1() {
+		return brand1;
+	}
+
+	public void setBrand1(String brand1) {
+		this.brand1 = brand1;
+	}
 
 }
