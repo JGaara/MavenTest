46a3a6b4cbcc5bc021bada65dfb8774a028c66e4
jiadawei <1225227815@qq.com>
2016-05-08 23:28:26 +0800

    bug修改，修改brand为brand1

--- src/main/java/com/jdw/bean/Car.java
+++ src/main/java/com/jdw/bean/Car.java
@@ -2,14 +2,14 @@ package com.jdw.bean;
 
 public class Car {
 
-	private String brand;
+	private String brand1;
 
-	public String getBrand() {
-		return brand;
+	public String getBrand1() {
+		return brand1;
 	}
 
-	public void setBrand(String brand) {
-		this.brand = brand;
+	public void setBrand1(String brand1) {
+		this.brand1 = brand1;
 	}
 
 }
