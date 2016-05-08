df2b2677304abc1aa27092dfdbe170dcf95cef2c
jiadawei <1225227815@qq.com>
2016-05-08 23:03:18 +0800

    Car.java添加brand属性

--- src/main/java/com/jdw/bean/Car.java
+++ src/main/java/com/jdw/bean/Car.java
@@ -1,5 +1,14 @@
 package com.jdw.bean;
 
 public class Car {
+	private String brand;
 
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	
 }
